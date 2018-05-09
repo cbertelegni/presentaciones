fb0ffd81cdc53ced373b4afab1cb9f4aefac9132
Cristian Bertelegni <cristianbertelegni@gmail.com>
2018-05-09 15:47:15 -0300

    source folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp   = require('gulp');
 var server = require('gulp-server-livereload');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('server', function() {
-  gulp.src('.')
+  gulp.src('source/')
     .pipe(server({
       livereload:       true,
       directoryListing: false,
@@ -11,4 +12,12 @@ gulp.task('server', function() {
       clientConsole:    true,
       port: 			8080
     }));
+});
+
+
+/** mahe and push github page branch from build folder */
+gulp.task('gh-page', function() {
+
+    return gulp.src('source/**/*')
+        .pipe(ghPages());
 });
